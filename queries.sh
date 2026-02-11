27a908e33db16b5d29fbc297a4c400591854e2d9
ntiyiso24 <ntiyisobelinda6@gmail.com>
2026-02-11 06:34:44 -0500

    Complete World Cup Database project

--- queries.sh
+++ queries.sh
@@ -35,7 +35,7 @@ echo -e "\nList of unique winning team names in the whole data set:"
 echo "$($PSQL "SELECT DISTINCT name FROM teams WHERE team_id IN (SELECT DISTINCT winner_id FROM games) ORDER BY name")"
 
 echo -e "\nYear and team name of all the champions:"
-echo "$($PSQL "SELECT year, name FROM games JOIN teams ON games.winner_id = teams.team_id WHERE games.round = 'Final' ORDER BY year" | sed 's/|/|/')"
+echo "$($PSQL "SELECT year, name FROM games JOIN teams ON games.winner_id = teams.team_id WHERE games.round = 'Final' ORDER BY year")"
 
 echo -e "\nList of teams that start with 'Co':"
 echo "$($PSQL "SELECT name FROM teams WHERE name LIKE 'Co%' ORDER BY name")"
